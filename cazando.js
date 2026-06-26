aa0dfa96442b6010dfb7fa3d624031801598cd89
Holger2005 <ordonezholger43@gmail.com>
2026-06-25 22:17:28 -0500

    Crear función graficarComida, iniciarJuego además definir varibles y constantes

--- cazando.js
+++ cazando.js
@@ -1,7 +1,26 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+const ALTO_GATO =30;
+const ANCHO_GATO = 100;
+const ALTO_COMIDA = 30;
+const ANCHO_COMIDA = 30;
 
+function iniciarJuego(){
+    gatoX = canvas.width/2 - ANCHO_GATO/2;
+    gatoY = canvas.height/2 - ALTO_GATO/2;
+    graficarGato();
+    graficarComida();
+}
 function graficarGato() {
     ctx.fillStyle = "blue";
-    ctx.fillRect(canvas.width/2 - 50, canvas.height/2 -15, 100, 30);
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
